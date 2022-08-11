bfc8bf32b688d0fcee920e8793dca16d764772ce
Brenden Thomas <brenden.thomas17@gmail.com>
2022-08-11 15:54:29 -0500

    Added CORS

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.use(cors())
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
